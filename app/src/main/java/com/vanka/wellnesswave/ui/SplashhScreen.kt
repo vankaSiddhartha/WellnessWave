a1ca6f4055bfdc10285ae7778b2a5702afda8c75
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2023-06-02 22:39:50 +0530

    Splash Screen Update

---
+++ app/src/main/java/com/vanka/wellnesswave/ui/SplashhScreen.kt
@@ -0,0 +1,12 @@
+package com.vanka.wellnesswave.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vanka.wellnesswave.R
+
+class SplashhScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashh_screen)
+    }
+}
